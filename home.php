de587e1ff956828158bcace88e38bf4259c20c65
Gustav <gubo0001@stud.kea.dk>
2025-09-11 11:20:36 +0200

    Implement code changes to enhance functionality and improve performance

---
+++ home.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <script src="app.js" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    <div id="container">
+        <nav>
+            <a href="">Twitter</a>
+            <a href="">Home</a>
+            <a href="">Explore</a>
+            <a href="">Notifications</a>
+            <a href="">Messages</a>
+            <a href="">Grok</a>
+            <a href="">Booksmarks</a>
+            <a href="">Jobs</a>
+            <a href="">Communities</a>
+            <a href="">Premium</a>
+            <a href="">Verified Orgs</a>
+            <a href="">Profile</a>
+            <a href="">More</a>
+            <button>Post</button>
+            <div>
+                Gustav Bøgh
+            </div>
+        </nav>
+
+        <main>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+        </main>
+
+        <aside>
+            <form action="">
+                <input type="text" placeholder="search">
+            </form>
+            <div class="likes">
+                <h3>Whats Happening</h3>
+                <div class="trending_item">
+                    <div>
+                        <p>Trending in Denmark</p>
+                        <p>Something great</p>
+                    </div>
+                    <div>...</div>
+                </div>
+                <div class="trending_item">
+                    <div>
+                        <p>Trending in Denmark</p>
+                        <p>Wild</p>
+                    </div>
+                    <div>...</div>
+                </div>
+                <div class="trending_item">
+                    <div>
+                        <p>Trending in Denmark</p>
+                        <p>Crazy</p>
+                    </div>
+                    <div>...</div>
+                </div>
+                <a href="">Show More</a>
+            </div>
+            <div class="trending">
+                <h3>Who to follow</h3>
+                <div class="follow_item">
+                    <div class="flex-box">
+                        <div>
+                            <img src="images/Cartoon-Man-Avatar-2.svg" alt="">
+                        </div>
+                        <div>
+                            <p>Hans Jensen</p>
+                            <p>@Somethinggreatname</p>
+                        </div>
+                    </div>
+                    <button>Follow</button>
+                </div>
+                <div class="follow_item">
+                    <div class="flex-box">
+                        <div>
+                            <img src="images/Cartoon-Man-Avatar-2.svg" alt="">
+                        </div>
+                        <div>
+                            <p>Fie Mikkelsen</p>
+                            <p>@howareyou</p>
+                        </div>
+                    </div>
+                    <button>Follow</button>
+                </div>
+                <div class="follow_item">
+                    <div class="flex-box">
+                        <div>
+                            <img src="images/Cartoon-Man-Avatar-2.svg" alt="">
+                        </div>
+                        <div>
+                            <p>Ole Thorstved</p>
+                            <p>@overvuderet</p>
+                        </div>
+                    </div>
+                    <button>Follow</button>
+                </div>
+                <a href="">Show More</a>
+            </div>
+        </aside>
+    </div>  
+</body>
+</html>
